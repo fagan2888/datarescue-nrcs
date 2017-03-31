6ac2517db48f373c9fa321d73f63bb2da558cc9d
Micah Smith <micahjsmith@gmail.com>
2017-03-31 17:30:17 -0400

    Add python package skeleton

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='nrcs_wcc_data',
+      version='0.0.1',
+      description='Utilities to download data from the NRCS WCC dataset',
+      url='http://github.com/micahjsmith/datarescue-nrcs',
+      license='MIT',
+      packages=['nrcs_wcc_data'])
